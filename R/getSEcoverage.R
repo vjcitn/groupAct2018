537b831ec65284514a038372c97d1ca7407a3de6
Vince Carey <stvjc@channing.harvard.edu>
2018-07-23 09:43:45 -0400

    initial commit

---
+++ R/getSEcoverage.R
@@ -0,0 +1,20 @@
+#' retrieve a covr report for SummarizedExperiment
+#' @export
+getSEcoverage = function() {
+ get(load(system.file("coverageObjs/SE_covr.rda", package="groupAct2018")))
+}
+#' retrieve a covr report for DESEQ2
+#' @export
+getDESEQ2coverage = function() {
+ get(load(system.file("coverageObjs/DESEQ2_covr.rda", package="groupAct2018")))
+}
+#' retrieve a covr report for tximport
+#' @export
+getTximportCoverage = function() {
+ get(load(system.file("coverageObjs/tximport_covr.rda", package="groupAct2018")))
+}
+#' retrieve a covr report for tximport
+#' @export
+getScranCoverage = function() {
+ get(load(system.file("coverageObjs/scran_coverage.rda", package="groupAct2018")))
+}
